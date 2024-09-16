98e562f982e08f5e9ce476be1d6ade9984023f7b
DanielBarie <73287620+DanielBarie@users.noreply.github.com>
2024-09-16 20:51:00 +0200

    changed telmate version

--- main.tf
+++ main.tf
@@ -2,8 +2,12 @@
 terraform {
   required_providers {
     proxmox = {
+      #source = "telmate/proxmox"
+      #version = "2.9.11"
       source = "telmate/proxmox"
-      version = "2.9.11"
+      version = "3.0.1-rc4"
+      #source = "TheGameProfi/proxmox"
+      #version = "2.10.0"
     }
   }
 }
@@ -13,14 +17,22 @@ provider "proxmox" {
   pm_api_token_secret = var.token_secret
   pm_tls_insecure = true
   pm_parallel = 2
+  pm_log_enable = true
+  pm_log_file   = "terraform-plugin-proxmox.log"
+  pm_debug      = true
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
   }
-resource "proxmox_vm_qemu" "rna2023" {
-  name = "rna2023-studvm-${count.index + 1}"
-  count = 40
-  vmid = count.index+1+600
+}
+resource "proxmox_vm_qemu" "psna-vi-gns3-2024" {
+  name = "psna-vi-gns3-2024-studvm-${count.index + 1}"
+  count = 41
+  #count = 2
+  vmid = count.index+1+500
   target_node = var.proxmox_host
   clone = var.template
-  ipconfig0 = "gw=172.23.255.254,ip=172.16.100.${count.index + 101}/13"
+  ipconfig0 = "gw=172.23.255.254,ip=172.16.200.${count.index + 101}/13"
   # wäre schön, geht aber nicht, weil das auf zfs nicht hinhaut?
   #full_clone  = "false"
   full_clone = "true"
@@ -30,17 +42,62 @@ resource "proxmox_vm_qemu" "rna2023" {
   sockets = 1
   cpu = "host"
   memory = 8192
-  #scsihw = "virtio-scsi-pci"
-  #bootdisk = "scsi0"
+  # die beiden folgenden einkommentiert
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+  #boot = "order=scsi0;ide2"
+  boot = "order=scsi0"
+  # neuen disks block, weil 
+  # das olle disk deprecated ist
+  # wir brauchen jetzt auch einen disk(s) block
+  # weil sonst das cloud init drive nicht richtig funktioniert
+  # und das setzen der boot order fehlschlägt (pve8 hat da  
+  # wohl den parameter check verbessert und lässt das nur für
+  # existierende disks zu (ohne disk(s) block existiert die bootplatte
+  # für pve halt nicht?)
+#  disk {
+#    slot = 4
+#    size = "64G"
+#    type = "scsi"
+#    storage = "storage-ssd-vmdata"
+    #discard = "on"
+#  }
 
-  #disk {
-  #  slot = 0
-  #  size = "G"
-  #  type = "scsi"
-  #  storage = "storage-ssd-vmdata"
-  #  discard = "on"
-  #}
+    disks {
+        scsi {
+            scsi0 {
+                disk {
+                    backup             = false
+                    cache              = "none"
+                    discard            = true
+                    emulatessd         = true
+                    size               = 64
+                    storage            = "storage-ssd-vmdata"
+                }
+            }
+	   scsi1 {
+		cloudinit {
+			storage        = "storage-ssd-vmdata"
+		}
+	   }
+        }
+        #ide {
+        #    ide4 {
+                #disk {
+                #    backup             = false
+                #    cache              = "none"
+                #    discard            = true
+                    #emulatessd         = true
+                #    size               = 4M
+                #    storage            = "storage-ssd-vmdata"
+                #}
+	#	cloudinit {
+	#		storage		= "storage-ssd-vmdata"
+	#	}
+        #    }
+        #}
 
+    }
   #network {
   #  model = "virtio"
   #  bridge = var.nic
